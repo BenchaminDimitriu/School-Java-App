5b0576b759b29d6ae76fd41b2c819aa8cb039049
D3153 <82058076+D3153@users.noreply.github.com>
2021-12-14 10:13:53 -0500

    finished buttons
    all menu buttons works

--- FinalProject/src/finalproject/TeacherMenuController.java
+++ FinalProject/src/finalproject/TeacherMenuController.java
@@ -7,13 +7,18 @@ package finalproject;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -74,5 +79,19 @@ public class TeacherMenuController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    @FXML
+     private void handleButtonAction(ActionEvent event) throws Exception{
+        Stage stage= null;
+        Parent root= null;
+        if(event.getSource() == btn_Back_To_Menu){ 
+            System.out.println("You are in Menu fxml");
+            stage = (Stage) btn_Back_To_Menu.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        }
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    } 
     
 }
