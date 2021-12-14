e03d2ca34aca223807d639a89be8b9fc7e0083ed
DeadBwain <82058228+DimitriuJonathan@users.noreply.github.com>
2021-12-14 09:55:35 -0500

    TeacherMenuController_Added

--- FinalProject/src/finalproject/TeacherMenuController.java
+++ FinalProject/src/finalproject/TeacherMenuController.java
@@ -7,27 +7,23 @@ package finalproject;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author Admin
  */
-public class StudentMenuController implements Initializable {
+public class TeacherMenuController implements Initializable {
 
+    @FXML
+    private VBox Teach_VBOX;
     @FXML
     private TextField tf_ID;
     @FXML
@@ -37,6 +33,12 @@ public class StudentMenuController implements Initializable {
     @FXML
     private TextField tf_Gender;
     @FXML
+    private TextField tf_Speciality;
+    @FXML
+    private TextField tf_Degree;
+    @FXML
+    private TextField tf_Dept_ID;
+    @FXML
     private Button btn_Add;
     @FXML
     private Button btn_Load;
@@ -44,6 +46,10 @@ public class StudentMenuController implements Initializable {
     private Button btn_Assign_Dept;
     @FXML
     private Button btn_Search;
+    @FXML
+    private Button btn_Assign_Dean;
+    @FXML
+    private TableView<?> tbl_Teach;
     @FXML
     private TableColumn<?, ?> cln_ID;
     @FXML
@@ -53,25 +59,13 @@ public class StudentMenuController implements Initializable {
     @FXML
     private TableColumn<?, ?> cln_Gender;
     @FXML
-    private TextField tf_Dept_ID;
+    private TableColumn<?, ?> cln_Speciality;
+    @FXML
+    private TableColumn<?, ?> cln_Degree;
     @FXML
     private TableColumn<?, ?> cln_Dept_ID;
     @FXML
     private Button btn_Back_To_Menu;
-    @FXML
-    private TextField tf_Speciality;
-    @FXML
-    private TextField tf_Degree;
-    @FXML
-    private Button btn_Assign_Dean;
-    @FXML
-    private VBox Teach_VBOX;
-    @FXML
-    private TableView<?> tbl_Teach;
-    @FXML
-    private TableColumn<?, ?> cln_Speciality;
-    @FXML
-    private TableColumn<?, ?> cln_Degree;
 
     /**
      * Initializes the controller class.
@@ -80,18 +74,5 @@ public class StudentMenuController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
-    private void handleButtonAction(ActionEvent event) throws Exception{
-        Stage stage= null;
-        Parent root= null;
-        if(event.getSource() == btn_Back_To_Menu){ 
-            System.out.println("You are in Menu fxml");
-            stage = (Stage) btn_Back_To_Menu.getScene().getWindow();
-            root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
-        }
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    } 
     
 }
